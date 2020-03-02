ca09ee47fc387cb63a7ef4d857de8c12336488ad
Grrzegorz Bonar <grzegorz.bonar@pracuj.pl>
2020-03-02 08:55:19 +0100

    OCP created

---
+++ Patterns/SOLID/Patterns.OCP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns.OCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
